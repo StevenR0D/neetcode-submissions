afd37a9c618b14b70310204c7c93a3242bfd8fb6
StevenR0D <136176728+StevenR0D@users.noreply.github.com>
2026-05-16 18:01:24 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        tracker_s = {}
+        tracker_t = {}
+        for i in range(len(s)):
+            if s[i] not in tracker_s:
+                tracker_s[s[i]]=1
+            else:
+                tracker_s[s[i]]+=1
+        for j in range(len(t)):
+            if t[j] not in tracker_t:
+                tracker_t[t[j]]=1
+            else:
+                tracker_t[t[j]]+=1
+        return tracker_t==tracker_s
